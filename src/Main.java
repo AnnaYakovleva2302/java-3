6ee55bc62db82e41d21f75547b1c63c995d71572
Anna Yakovleva <_bel4onok_@mail.ru>
2021-08-14 21:23:53 +0300

    Create program for miles calculation

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        
+        double priceTicket = 7_002.70;
+        int priceForOneMile = 20;
+        long miles = (long) priceTicket / priceForOneMile;
+        System.out.println("Bonus miles: " + miles);
     }
 }
